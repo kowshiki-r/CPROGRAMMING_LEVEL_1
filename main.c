f7c0d0e1495491f4735a3045fae8a839e86574ee
kowshiki-r <kowshikir23@gmail.com>
2024-10-04 09:20:28 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2;
+    printf("Enter the number");
+    scanf("%d",&num1);
+    num2=num1+2;
+    printf("%d",num2);
+    return 0;
+}
