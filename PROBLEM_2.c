ad44d27b791f118cd1ff3f24df0822d612efbfae
kowshiki-r <kowshikir23@gmail.com>
2025-02-09 14:26:03 +0530

    Create PROBLEM_2.c

---
+++ PROBLEM_2.c
@@ -0,0 +1,10 @@
+//GET A NUMBER FROM USER AND SUBTRACT 5 TO THAT NUMBER AND PRINT THE RESULT INPUT:45 OUTPUT:40
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number");
+    scanf("%d",&num);
+    num=num-5;
+    printf("%d",num);
+}
