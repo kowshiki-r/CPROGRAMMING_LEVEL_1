a12c13213ca5c435c480097ce352228c240edd90
kowshiki-r <kowshikir23@gmail.com>
2025-02-09 14:20:47 +0530

    Create PROBLEM_1.c

---
+++ PROBLEM_1.c
@@ -0,0 +1,11 @@
+//GET A NUMBER FROM USER AND ADD 2 THAT NUMBER PRINT THE RESULT INPUT:5 OUTPUT:7
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter number");
+    scanf("%d",&num);
+    num=num+2;
+    printf("%d",num);
+
+}
